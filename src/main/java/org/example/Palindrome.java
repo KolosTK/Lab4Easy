170f9106010b04c9a3bb69171b5f46f281a3f707
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-21 13:27:10 +0200

    Create Palindrome class

---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Palindrome {
+    Palindrome (String word){
+
+    }
+    public boolean IsWordPalindrome(String word)
+    {
+        word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int begin=0;
+        int end = word.length()-1;
+
+        while (begin < end) {
+            if (word.charAt(begin) != word.charAt(end)) {
+                return false;  // It's not a palindrome if there's a mismatch
+            }
+            begin++;
+            end--;
+        }
+
+        return false;
+    }
+}
